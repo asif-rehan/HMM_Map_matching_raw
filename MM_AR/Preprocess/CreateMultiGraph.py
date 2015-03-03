8daf83adccdf4abc793eb200a8ad69ce4d4f7a84
Asif Rehan <asif.rehan9@gmail.com>
2015-03-03 00:34:04 -0500

    CreateMultiGraph.py now has another func, CreateMultiDiGraph() that creates a MultiDiGraph.p in MM_AR/Relevant_files directory

--- MM_AR/Preprocess/CreateMultiGraph.py
+++ MM_AR/Preprocess/CreateMultiGraph.py
@@ -20,6 +20,21 @@ def CreateMultiGraph(utm_shp_path):
     #plt.show()'''
     return G
 
+def CreateMultiDiGraph(utm_shp_path):
+    G = nx.MultiDiGraph()
+    with fiona.open(utm_shp_path,crs= from_epsg(32618)) as shp:
+        #,driver='ESRI Shapefile'
+        for elem in shp:
+            strt = elem['geometry']['coordinates'][0]
+            len_coords = len(elem['geometry']['coordinates'])
+            end = elem['geometry']['coordinates'][len_coords-1]
+            length_meter = elem['properties']['Length_met']
+            G.add_edge(strt, end, weight = length_meter)
+            G.add_edge(end, strt, weight = length_meter)                                    
+    '''print len(G), G.number_of_nodes(),G.number_of_edges()
+    nx.draw_graphviz(G)                    
+    plt.show()'''
+    return G
 utm_shp_path = os.path.join(r'C:\Users\asr13006\Google Drive\UConn MS',
                             r'Py Codes\HMM_Krumm_Newson_Implementation\MM_AR',
                             r'Relevant_files\LineString_Road_Network_UTM.shp')
@@ -34,14 +49,16 @@ utm_shp_path = os.path.join(r'C:\Users\asr13006\Google Drive\UConn MS',
 #routes/County 13/road network micro/nxspatial3/NF/copy/TransformCRS/\
 #LineString_Road_Network_UTM.shp" 
 
-G = CreateMultiGraph(utm_shp_path)
-pickle.dump(G, open('../Relevant_files/MultiGraph.p', 'wb'))
-
-#print nx.dijkstra_path(G, (732805.0271117099, 4623614.515877712),
-#                              (729658.6308007092, 4630691.33944068), 'weight')
-
-#print G.neighbors((729658.6308007092, 4630691.33944068))
-#print G.neighbors((732805.0271117099, 4623614.515877712))
-
-#print G.degree([(732805.0271117099, 4623614.515877712),(732805.0271117099, 4623614.515877712)])
-
+if __name__ == '__main__':
+    #G = CreateMultiGraph(utm_shp_path)
+    #pickle.dump(G, open('../Relevant_files/MultiGraph.p', 'wb'))
+    MDG = CreateMultiDiGraph(utm_shp_path)
+    pickle.dump(MDG, open('../Relevant_files/MultiDiGraph.p', 'wb'))
+    #print nx.dijkstra_path(G, (732805.0271117099, 4623614.515877712),
+    #                              (729658.6308007092, 4630691.33944068), 'weight')
+    
+    #print G.neighbors((729658.6308007092, 4630691.33944068))
+    #print G.neighbors((732805.0271117099, 4623614.515877712))
+    
+    #print G.degree([(732805.0271117099, 4623614.515877712),(732805.0271117099, 4623614.515877712)])
+    
--- MM_AR_validation/script_for_val_plots.py
+++ MM_AR_validation/script_for_val_plots.py
@@ -6,23 +6,95 @@ Created on Feb 13, 2015
 import os
 import pandas as pd
 from matplotlib import pyplot as plt
-out_fldr = r'C:\\Users\\asr13006\\Google Drive\\UConn MS\\Py Codes/HMM_Krumm_Newson_Implementation\\MM_AR_validation\\val_dataset\\val_dataset_output'
-out = os.path.join(out_fldr, "quant_val_out2.csv")
-x = pd.read_csv(out)
-x = x.drop(x.index[[9,18]])
-x = x.drop('Unnamed: 0', 1)
+out_fldr = os.path.join(r'C:\\Users\\asr13006\\Google Drive\\UConn MS',
+            r'Py Codes/HMM_Krumm_Newson_Implementation\\MM_AR_validation',
+            r'val_graphs')
+#out = os.path.join(out_fldr, "quant_val_out2.csv")
+#x = pd.read_csv(out)
+#x = x.drop(x.index[[9,18]])
+#x = x.drop('Unnamed: 0', 1)
 
-fig = plt.figure()
-ax = plt.axes()
 
-for cat in x.Freq.unique():
-    tod_data = x[x.Freq == cat]
-    ax.plot(tod_data['Error'].values, label=cat)
-    print tod_data
-    plt.title('Error for various Freq')
-    plt.legend()
-plt.savefig(os.path.join(r'C:\Users\asr13006\Google Drive\UConn MS',
-                             r'Py Codes\HMM_Krumm_Newson_Implementation',
-                             r'MM_AR_validation\val_graphs', \
-                             'bl_freq_error.png'))
+seg1 = 'server1_output_20150225T232216.csv'
+seg2 = 'server2_output_20150226T072402.csv'
+df1 = pd.read_csv(os.path.join(out_fldr, seg1))
+df2 = pd.read_csv(os.path.join(out_fldr, seg2))
+all_bt4 = df1.append(df2)
+'''
+for cat in ['Freq' ,'Route', 'DOW', 'TOD']:
+    for metric in ['Error', 'Score']:
+        fig = plt.figure()
+        ax = plt.axes()
+        plt.title('{} for {}'.format(metric, cat) )
+        for uniq in all_bt4[cat].unique():
+            seg_data = all_bt4[all_bt4[cat] == uniq]
+            ax.plot(seg_data[metric].values, label=uniq)
+            print seg_data.head(2)
+            print  seg_data.tail(2)  
+            
+            plt.legend()
+        plt.savefig(os.path.join(r'C:\Users\asr13006\Google Drive',
+                        r'UConn MS\Py Codes\HMM_Krumm_Newson_Implementation',
+                        r'MM_AR_validation\val_graphs', 
+                        '{}_{}.png'.format(metric, cat)))
+        plt.show()'''
+
+col = {'bl': 'b', 'rd':'r', 'gr':'g', 'yl':'y', 'or':'#FFA500','pl':'#7F00FF'}
+mar = {'bl': 'x', 'rd':'+', 'gr':'d', 'yl':'s', 'or':'o','pl':'^'}
+leg = {'bl': 'Blue Line', 'rd':'Red Line', 'gr':'Green Line', 
+          'yl':'Yellow Line', 'or':'Orange Line','pl':'Purple Line'}
+def plt_err_vs_freq_on_route(xvar, yvar, var3):
+    uniq_var3 = all_bt4[var3].unique()
+    for uniq in uniq_var3:
+        subset = all_bt4[all_bt4[var3] == uniq]
+        plt.scatter(subset[xvar], subset[yvar], 
+             color=col[uniq], marker=mar[uniq], alpha=0.5, label=leg[uniq])
+    plt.legend(loc='lower right')
+    plt.axhline(y=0,xmin=0,xmax=65,c="k",linewidth=0.5,zorder=0)
+    plt.axhline(y=100,xmin=0,xmax=65,c="k",linewidth=0.5,zorder=0)
+    plt.axhline(y=-100,xmin=0,xmax=65,c="k",linewidth=0.5,zorder=0)
+    plt.title('%{} vs Frequency for {}'.format(yvar, var3))
+    plt.xlabel('Frequency')
+    plt.ylabel('%{}'.format(yvar))
+    plt.show()
+    
+plt_err_vs_freq_on_route(xvar='Freq', yvar='Error', var3='Route')
+plt_err_vs_freq_on_route(xvar='Freq', yvar='Score', var3='Route')
+
+col = {'tue':'#2eb7ed', 'wed':'#fee529', 'thu':'#cd4236'}
+mar = {'tue':'^', 'wed':'o', 'thu':'s'}
+leg = {'tue':'Tuesday', 'wed':'Wednesday', 'thu':'Thursday'}
+for dow in all_bt4['DOW'].unique():
+        subset = all_bt4[all_bt4['DOW'] == dow]
+        plt.scatter(subset['Freq'], subset['Error'], 
+             color=col[dow], marker=mar[dow], label=leg[dow])
+plt.legend()
+plt.axhline(y=0,xmin=0,xmax=65,c="k",linewidth=0.5,zorder=0)
+plt.title('%Error vs Frequency for Dates of Week')
+plt.xlabel('Frequency')
+plt.ylabel('%Error')
 plt.show()
+plt_err_vs_freq_on_route(xvar='Freq', yvar='Score', var3='DOW')
+for tod in all_bt4['TOD'].unique():
+        subset = all_bt4[all_bt4['TOD'] == tod]
+        plt.scatter(subset['Freq'], subset['Error'], 
+             color=col[tod], marker=mar[tod], label=leg[tod], alpha=0.5)
+plt.legend()
+plt.axhline(y=0,xmin=0,xmax=65,c="k",linewidth=0.5,zorder=0)
+plt.title('%Error vs Frequency for Time of Day')
+plt.xlabel('Frequency')
+plt.ylabel('%Error')
+plt.show()
+
+
+plt_err_vs_freq_on_route(xvar='Freq', yvar='Score', var3='TOD')
+
+
+
+
+
+
+
+
+
+
